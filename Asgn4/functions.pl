e35ee4d6fb13a18d40677a22b3f9419de289bfa0
Edward Nguyen <edwardn2147@yahoo.com>
2017-06-03 18:35:29 -0700

    added logic for flight arrival time

--- Asgn4/functions.pl
+++ Asgn4/functions.pl
@@ -22,10 +22,24 @@ dms_to_radian( degree, minutes, radian) :-
 
 /*calculate flight time from one airport to another? */
 
-find_arrival_time(departure_time, distance, arrival_time) :-
+find_arrival_time(time(depart_hours, depart_minutes), distance,
+        time(arrival_hours, arrival_minutes)) :-
 	travel_time is distance / 500,
 	/* convert travel hours to date time format */
 	travel_hours is floor(travel_time),
-	minutes is (travel_time - travel_hours) / 60,
+	minutes is (travel_time - travel_hours) * 60,
 	travel_minutes is floor(minutes),
-	 
+    arrival_hours is depart_hours + travel_hours,
+    arrival_minutes is depart_minutes + travel_minutes,
+    calibrate(arrival_hours, arrival_minutes),
+    arrival_time = time(arrival_hours, arrival_minutes),
+    write(arrival_time), nl.
+
+calibrate(hours, minutes) :-
+    (   minutes > 59 ->
+        minutes is minutes - 60,
+        hours is hours + 1,
+    ;
+    ).
+
+
